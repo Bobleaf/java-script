8d93ca19eee65e9d17a1b17b85382d51105343f6
kaike santana <santanadeabreukaike@gmail.com>
2024-10-21 21:15:23 -0300

    atualização

---
+++ quarta/aula4.js
@@ -0,0 +1,30 @@
+//OPERADORES JAVASCRIPT
+
+// + = ADIÇÃO
+// - = SUBTRAÇÃO
+// / = DIVIDIR
+// * = MULTIPLICAÇÃO
+
+const resultado = 7 + 3;
+
+console.log(resultado);
+
+const resultadoSubtraco = 9 - 5;
+
+console.log(resultadoSubtraco);
+
+const resultadoDivisao = 25 / 5;
+
+console.log(resultadoDivisao)
+
+const resultadoMUltiplicacao = 8 * 8;
+
+console.log(resultadoMUltiplicacao);
+
+//Podemos usar esses operadores em outras situações, como incrementar uma frase por exemplo.
+
+const text1 = 'olá, meu nome é kaike '
+const text2 = 'eu sou aluno da obra social dom bosco'
+
+const textoFinal = text1 + text2
+console.log(textoFinal)
---
+++ terceira/aula3.js
@@ -0,0 +1,19 @@
+//(const) usado para declarar uma variável de valor fixo, ou seja que nunca será alterada.
+
+//(let) é usado para declarar uma varíavel de valor não fixo, ou seja, podemos a qualquer momento alterar o valor dela.
+
+let mensagemTela = ('este texto é um exemplo para podermos praticar o exercicio');
+
+//console.log(mensagemTela);
+
+mensagemTela = ('aqui estou alterando o valor da minha variável topo let')
+
+//console.log(mensagemTela)
+
+const mensagemExemplo = ("Essa mensagem é um teste");
+
+//console.log(mensagemExemplo);
+
+mensagemExemplo = ("estou alterando o texto para teste");
+
+console.log(mensagemExemplo);
