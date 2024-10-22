e50f8ba86b974565b681dbf80a54d4cf95fcdfc8
kaike santana <santanadeabreukaike@gmail.com>
2024-10-21 21:34:17 -0300

    atualização

---
+++ sexta/aula6.js
@@ -0,0 +1,15 @@
+//OBJETO NO JAVASCRIPT.
+
+const Carta = {
+    nome: 'Pikachu',
+    habìlidade: 95,
+    defesa: 40,
+    ataque: 89,
+    slogan: 'Pikachu e raio'
+};
+
+console.log(`Meu personagem se chama: ${Carta.nome}`);
+console.log(`O ataque do meu personagem é: ${Carta.habìlidade}`);
+console.log(`A defesa do meu personagem é: ${Carta.defesa}`);
+console.log(`O ataque do meu personagem é: ${Carta.ataque}`);
+console.log(`O meu tipo é ${Carta.slogan}`);
