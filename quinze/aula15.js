082786ad6a4c7559bb4599439b5d9edae4ff0bfb
kaike santana <santanadeabreukaike@gmail.com>
2024-11-01 20:56:57 -0300

    atualização

---
+++ quinze/aula15.js
@@ -0,0 +1,8 @@
+const typed = new Typed('#pokemon', {
+    strings: ["Historia do pokemon", "Como foi criado"],
+    typeSpeed: 50,//VELOCIDADE QUE SERÁ DIGITADO NOSSA TEXT
+    backSpeed: 25,//VOCIDADE DE RETORNO DO TEXT
+    loop: true, //fas com que o texto fique em execução infinita
+    showCursor: false,//estamos removendo o cursor o padrão do HTML
+    //fadeOut: true,//adiciona uma transição OPICIONAL
+}); 
